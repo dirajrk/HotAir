f33df4beb22ae578e915e216b2072caba8d39b9e
Diraj Ravikumar <jc346295@ad.jcu.edu.au>
2017-04-26 13:43:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jc346295/hotair/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.jc346295.hotair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
+
