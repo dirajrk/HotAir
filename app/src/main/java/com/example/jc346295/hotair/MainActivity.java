c2e90136f7b6b7239f925e7a15aebed4dbc9a22d
fadni <fadnihdr@gmail.com>
2017-04-26 16:28:00 +0800

    new intent from main to report

--- app/src/main/java/com/example/jc346295/hotair/MainActivity.java
+++ app/src/main/java/com/example/jc346295/hotair/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.jc346295.hotair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton reportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void onClick(View view){
+        reportButton = (ImageButton) findViewById(R.id.reportButton);
+        if (view == reportButton){
+            Intent intent = new Intent(this, Report.class);
+            startActivity(intent);
+        }
     }
 }
