0a27fc51749fea0c524c5a5c42d349a28d1cff61
fadni <fadnihdr@gmail.com>
2017-04-26 14:46:28 +0800

    main activity halfway done

--- app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
+++ app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.jc346295.hotair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -16,7 +17,8 @@ public class LoginActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/jc346295/hotair/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jc346295.hotair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
