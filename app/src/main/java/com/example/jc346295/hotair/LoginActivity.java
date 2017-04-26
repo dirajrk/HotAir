fcc9eb93657763b0861f336c518bc5508af219a1
fadni <fadnihdr@gmail.com>
2017-04-26 13:54:39 +0800

    main_activity to login_activity
    'text' to 'hint' in login_activity

--- app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
+++ app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
@@ -4,15 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
-import android.widget.Toast;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
