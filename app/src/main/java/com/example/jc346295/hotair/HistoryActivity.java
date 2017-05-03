489a6aa3040eff0751748fcce7cba0dd1f406763
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2017-05-03 13:23:12 +0800

    Order History

---
+++ app/src/main/java/com/example/jc346295/hotair/HistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.jc346295.hotair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
