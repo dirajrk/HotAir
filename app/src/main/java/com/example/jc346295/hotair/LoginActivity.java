81af36c3ff999fb9d68e9896444fcbb00cb6143d
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2017-05-03 13:35:47 +0800

    Fixed Login Activity

--- app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
+++ app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
@@ -5,29 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
-import android.content.SharedPreferences;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button button;
-    SharedPreferences preferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void onClick(View view){
-        Button button = (Button) findViewById(R.id.button);
-        //both these values are placeholders
-        String userID = "";
-        String userPass = "";
-        if (view == button){
-            Intent intent = new Intent(this, MainActivity.class);
-            preferences.edit().clear().putString("id",userID).putString("pass",userPass).apply();
-            startActivity(intent);
-        }
-    }
 }
 
