e9e80b2248bdd4b48204aef85bde9f9c51d43720
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2017-05-07 15:44:57 +0800

    MapView test

--- app/src/main/java/com/example/jc346295/hotair/TrackActivity.java
+++ app/src/main/java/com/example/jc346295/hotair/TrackActivity.java
@@ -3,6 +3,8 @@ package com.example.jc346295.hotair;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.MapView;
+
 
 public class TrackActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class TrackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
+        //fetch the map view from the layout
+        MapView mapView = (MapView) findViewById(R.id.mapview);
     }
 }
