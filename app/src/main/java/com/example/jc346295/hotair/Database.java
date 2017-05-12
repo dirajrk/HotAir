9acc19cabd84cedab35c65e54768d94ee6f3e3d6
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2017-05-12 17:04:04 +0800

    Trying to implement content resolver and provider

---
+++ app/src/main/java/com/example/jc346295/hotair/Database.java
@@ -0,0 +1,60 @@
+package com.example.jc346295.hotair;
+
+/**
+ * Created by Fadni Harisyam Daeng Rani on 12/5/17.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+    public static final String USER_TABLE_NAME = "user";
+    public static final int ID_COLUMN = 0;
+    public static final int USERNAME_COLUMN = 1;
+    public static final int PASS_COLUMN = 2;
+    public static final int ORDERS_COLUMN = 3;
+    public static final String ID = "_id";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "pass";
+    public static final String ORDERS = "order";
+    private final static int VERSION = 6;
+
+
+    public Database(Context context) {
+        super(context, "userCred.db", null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        setup(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE_NAME + ";");
+        setup(db);
+    }
+
+    private void setup(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + USER_TABLE_NAME
+                + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + USERNAME + " TEXT, "
+                + PASSWORD + " TEXT, "
+                + ORDERS + " INTEGER);";
+        db.execSQL(sql);
+
+        sql = "INSERT INTO user (username,pass) VALUES (nathanaeln,baygon1);";
+        db.execSQL(sql);
+
+        sql = "INSERT INTO user (username,pass) VALUES (fadnihdr,1234);";
+        db.execSQL(sql);
+
+        sql = "INSERT INTO user (username,pass) VALUES (dirajrk,9999);";
+        db.execSQL(sql);
+    }
+
+}
+
--- app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
+++ app/src/main/java/com/example/jc346295/hotair/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.example.jc346295.hotair;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private Database userCred;
     private Button button;
+    private EditText userName;
+    private EditText userPass;
 
 
     @Override
@@ -15,17 +22,51 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userCred = new Database(this);
+        SQLiteDatabase db = userCred.getReadableDatabase();
+
+
+        userName = (EditText) findViewById(R.id.userEmail);
+        userPass = (EditText) findViewById(R.id.userPassword);
+
+        final String input1 = userName.getText().toString();
+        String input2 = userPass.getText().toString();
+        String[] names = {"Jack Smith"};
+
+
+        String selection = Database.USERNAME + " = ?";
+        String[] selectionArgs = {"Jack Smith"};
+
+        Cursor cursor = getContentResolver().query();
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
+                while (cursor.moveToNext()) {
+                    String name = cursor.getString(Database.USERNAME_COLUMN);
+                    if (name.equals(input1)) {
+                        cursor.close();
+                        userCred.close();
+                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        new AlertDialog.Builder(view.getContext())
+                                .setTitle("No username")
+                                .setMessage("You'e not cool enough to be in the database!")
+                                .show();
+                    }
+                }
+
             }
         });
 
     }
-
 }
-
